f6d18c5c5c30cc141627b9d14a38cfd585bd0836
Alaskarli <nazrinna@code.edu.az>
2023-01-26 17:56:30 -0800

    inheritance

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿
+
+
+//using System;
+
+//int number = -40;
+//if (number > 0)
+//{
+//    Console.WriteLine($"{number}- musbet eddedir");
+//}
+//else if (number == 0)
+//{
+//    Console.WriteLine("{number}");
+//}
+//else
+//{
+//    Console.WriteLine($"{number}-menfi eddedir");
+//}
+
+//int[] numbers = { 1, 2, 3, 4, -1 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > 0)
+//    {
+//        Console.WriteLine($"{numbers[i]}-musbet ededdir");
+//    }
+//    else if (numbers[i] == 0)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{numbers[i]}-menfi ededir");    
+//}   }
+
+
+//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//int[] arr = { 100, 0, -40, 323 };
+//foreach (int i in arr)
+//{
+//    CheckNumber(arrItem);
+//}
+
+
+//        }
+
+//    }
+//       static void CheckNumber(int number)
+//{
+//    if (number > 0)
+//    {
+//        Console.WriteLine($"{number}- musbet eddedir");
+//    }
+//    else if (number == 0)
+//    {
+//        Console.WriteLine("{number}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{number}-menfi eddedir");
+//    }
+//}
+
+        static void Main()
+        {
+            int[] arr = { 100, 0, -40, 323 };
+              foreach (var arrItem in arr)
+              {
+                CheckNumber(arrItem);
+              }
+        }
+        static void CheckNumber(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"{number}- musbet eddedir");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("{number}");
+            }
+            else
+            {
+                Console.WriteLine($"{number}-menfi eddedir");
+
+
+            }
+        }
+    
+
+       
+
+
+
+
+
